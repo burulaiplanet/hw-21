f14af6cd63156f51a86946c83d2bd0a964615b10
unknown <burulaiit>
2022-05-28 12:46:53 +0600

    update

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,54 +1,62 @@
-import { useReducer} from 'react'
-
-
+import { useReducer } from 'react'
 
 const inputChangeReducer = (prevState, action) => {
 	if (action.type === 'ONE_INPUT') {
 		return {
 			...prevState,
 			firstNameValue: action.firstNameValue,
-			isValidOneInput: prevState.firstNameValue.length < 8,
+			isValidOneInput: action.firstNameValue.length < 8,
 		}
 	}
 	if (action.type === 'TWO_INPUT') {
 		return {
 			...prevState,
 			lastNameInputValue: action.lastNameInputValue,
-			isValidTwoInput: prevState.lastNameInputValue.length <9,
+			isValidTwoInput: action.lastNameInputValue.length < 9,
 		}
 	}
 	if (action.type === 'THREE_INPUT') {
-    // console.log(prevState.emailInputValue.includes('@'));
 		return {
 			...prevState,
 			emailInputValue: action.emailInputValue,
-			isValiThreeInput: action.emailInputValue.includes('@'),
+			isValidThreeInput: action.emailInputValue.includes('@'),
 		}
 	}
 
-	if (action.type === 'INPUTS_BLUR')
+	if (action.type === 'FIRST_BLUR')
 		return {
 			...prevState,
-			isValidOneInput: prevState.firstNameValue.length <8,
-			isValidTwoInput: prevState.lastNameInputValue.length <9,
-			isValiThreeInput: prevState.emailInputValue.includes('@'),
+			isValidOneInput:
+				prevState.firstNameValue.length > 0 &&
+				prevState.firstNameValue.length < 8,
+		}
+	if (action.type === 'LASTNAME_BLUR')
+		return {
+			...prevState,
+
+			isValidTwoInput:
+				prevState.lastNameInputValue.length > 0 &&
+				prevState.lastNameInputValue.length < 9,
+		}
+	if (action.type === 'EMAIL_BLUR')
+		return {
+			...prevState,
+			isValidThreeInput: prevState.emailInputValue.includes('@'),
 		}
 	return prevState
-
 }
 
 const BasicForm = () => {
 	const [inputState, dispatchInputState] = useReducer(inputChangeReducer, {
-		isValidOneInput: '',
+		isValidOneInput: true,
 		firstNameValue: '',
 
-		isValidTwoInput: '',
+		isValidTwoInput: true,
 		lastNameInputValue: '',
 
-		isValiThreeInput: '',
+		isValidThreeInput: true,
 		emailInputValue: '',
 	})
-
 
 	const oneInput = (event) => {
 		dispatchInputState({
@@ -69,9 +77,19 @@ const BasicForm = () => {
 		})
 	}
 
-	const validateIputsHandler = () => {
+	const validateFirstHandler = () => {
 		dispatchInputState({
-			type: 'INPUTS_BLUR',
+			type: 'FIRST_BLUR',
+		})
+	}
+	const validatelastNameHandler = () => {
+		dispatchInputState({
+			type: 'LASTNAME_BLUR',
+		})
+	}
+	const emailBlurValidate = () => {
+		dispatchInputState({
+			type: 'EMAIL_BLUR',
 		})
 	}
 
@@ -83,25 +101,27 @@ const BasicForm = () => {
 		<form onSubmit={submitHandler}>
 			<div className='control-group'>
 				<div
-					className={`${['form-control']} ${
+					className={
 						inputState.isValidOneInput === false
-							? ['form-control invalid']
-							: ['form-control']
-					}`}
+							? 'form-control invalid'
+							: 'form-control'
+					}
 				>
 					<label htmlFor='name'>First Name</label>
 					<input
 						onChange={oneInput}
 						type='text'
 						id='name'
-						onBlur={validateIputsHandler}
+						onBlur={validateFirstHandler}
 					/>
-					 {!inputState.isValidOneInput && <p>Should not be more than eight characters</p>}
+					{!inputState.isValidOneInput && (
+						<p>Should not be more than eight characters</p>
+					)}
 				</div>
 				<div
 					className={
 						inputState.isValidTwoInput === false
-							? ['form-control invalid']
+							? 'form-control invalid'
 							: 'form-control '
 					}
 				>
@@ -110,15 +130,17 @@ const BasicForm = () => {
 						onChange={twoInput}
 						type='text'
 						id='name'
-						onBlur={validateIputsHandler}
+						onBlur={validatelastNameHandler}
 					/>
-					{!inputState.isValidTwoInput && <p>should not be more than nine characters</p>}
+					{!inputState.isValidTwoInput && (
+						<p>should not be more than nine characters</p>
+					)}
 				</div>
 			</div>
 			<div
 				className={
-					inputState.isValiThreeInput === false
-						? ['form-control invalid']
+					inputState.isValidThreeInput === false
+						? 'form-control invalid'
 						: 'form-control '
 				}
 			>
@@ -127,16 +149,13 @@ const BasicForm = () => {
 					onChange={threeInput}
 					type='text'
 					id='name'
-					onBlur={validateIputsHandler}
+					onBlur={emailBlurValidate}
 				/>
 
-				{!inputState.isValiThreeInput && <p>Not entered @</p>}
-
-				{!inputState.isValidTwoInput && <p>Not entered @</p>}
-
+				{!inputState.isValidThreeInput && <p>Not entered @</p>}
 			</div>
 			<div className='form-actions'>
-				<button >Submit</button>
+				<button>Submit</button>
 			</div>
 		</form>
 	)
